1079f329c2392a643a9735a13473e2104d3aab9e
zhang <john@john-PC>
2020-04-22 15:52:38 +0800

    去掉注释

--- src/main/java/com/xcx/git_test/Test.java
+++ src/main/java/com/xcx/git_test/Test.java
@@ -1,13 +1,9 @@
 package com.xcx.git_test;
 
-/**
- * Hello world!
- *
- */
 public class Test 
 {
     public static void main( String[] args )
-    {
+    {  
         System.out.println( "Hello World!" );
     }
 }
